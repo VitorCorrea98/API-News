4d708aa8d941c349b371ee285185e5223ab9d88a
Vitor Correa <vrcnode@gmail.com>
2023-08-11 09:11:17 -0300

    Deploy pronto

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="text-white text-2xl flex justify-evenly p-4 items-center mt-auto">
       <p>Copyright (c) 2023 The New York Times Company. All Rights Reserved.</p>
       <a href="https://developer.nytimes.com." target="_blank" rel="noreferrer">
-        <img src="./Images/API-Logo2.png" alt="API Logo" />
+        <img src="/Images/API-Logo2.png" alt="API Logo" />
       </a>
     </footer>
   );
