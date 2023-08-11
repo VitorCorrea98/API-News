593017cec0a4738f956f3e9a0ca1ed755a88115f
Vitor Correa <vrcnode@gmail.com>
2023-08-11 09:13:52 -0300

    tudo feito

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/VitorCorrea98/API-News'
+  base: '/API-News'
 })
 
