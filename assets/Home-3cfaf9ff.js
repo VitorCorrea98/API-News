5481e27fe0ef6ea24a3795dae2245801e9997a79
Vitor Correa <vrcnode@gmail.com>
2023-08-11 09:20:25 -0300

    Updates

---
+++ assets/Home-3cfaf9ff.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-2f0c72e7.js";import{S as s}from"./Stories-b290f29b.js";const m=()=>{const r="movies";return o.jsx(o.Fragment,{children:o.jsx(s,{category:r})})};export{m as default};
---
+++ assets/Home-f1800e31.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-2f0c72e7.js";import{S as t}from"./Stories-b290f29b.js";const m=()=>{const r="world";return o.jsx(o.Fragment,{children:o.jsx(t,{category:r})})};export{m as default};
